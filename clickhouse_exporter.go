58786bb30d784d3a300f4a3fa18777387eab1566
smh_tabatabaei <tabatabei.smh@yaftar.ir>
2025-05-25 17:08:02 +0330

    feat: getting clickhouseScrapURI from env not command arg

--- clickhouse_exporter.go
+++ clickhouse_exporter.go
@@ -15,9 +15,9 @@ import (
 var (
 	listeningAddress    = flag.String("telemetry.address", ":9116", "Address on which to expose metrics.")
 	metricsEndpoint     = flag.String("telemetry.endpoint", "/metrics", "Path under which to expose metrics.")
-	clickhouseScrapeURI = flag.String("scrape_uri", "http://localhost:8123/", "URI to clickhouse http endpoint")
 	clickhouseOnly      = flag.Bool("clickhouse_only", false, "Expose only Clickhouse metrics, not metrics from the exporter itself")
 	insecure            = flag.Bool("insecure", true, "Ignore server certificate if using https")
+	clickhouseScrapeURI = os.Getenv("CLICKHOUSE_URI")
 	user                = os.Getenv("CLICKHOUSE_USER")
 	password            = os.Getenv("CLICKHOUSE_PASSWORD")
 )
@@ -25,11 +25,11 @@ var (
 func main() {
 	flag.Parse()
 
-	uri, err := url.Parse(*clickhouseScrapeURI)
+	uri, err := url.Parse(clickhouseScrapeURI)
 	if err != nil {
 		log.Fatal().Err(err).Send()
 	}
-	log.Printf("Scraping %s", *clickhouseScrapeURI)
+	log.Printf("Scraping %s", clickhouseScrapeURI)
 
 	registerer := prometheus.DefaultRegisterer
 	gatherer := prometheus.DefaultGatherer
