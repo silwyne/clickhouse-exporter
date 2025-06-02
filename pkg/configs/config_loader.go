42213c913a009542414ea51afc791791b79f8889
smh_tabatabaei <tabatabei.smh@yaftar.ir>
2025-06-02 10:59:43 +0330

    feat: added QueryFiltersPath variable to configurations

--- pkg/configs/config_loader.go
+++ pkg/configs/config_loader.go
@@ -6,24 +6,30 @@ import (
 )
 
 type Configuration struct {
-	ListeningAddress    *string
-	MetricsEndpoint     *string
-	ClickhouseOnly      *bool
-	Insecure            *bool
+	ListeningAddress *string
+	MetricsEndpoint  *string
+	ClickhouseOnly   *bool
+	Insecure         *bool
+
 	ClickhouseScrapeURI string
 	User                string
 	Password            string
+
+	QueryFiltersPath string
 }
 
 func LoadConfigs() Configuration {
 	configs := Configuration{
-		ListeningAddress:    flag.String("telemetry.address", ":9116", "Address on which to expose metrics."),
-		MetricsEndpoint:     flag.String("telemetry.endpoint", "/metrics", "Path under which to expose metrics."),
-		ClickhouseOnly:      flag.Bool("clickhouse_only", false, "Expose only Clickhouse metrics, not metrics from the exporter itself"),
-		Insecure:            flag.Bool("insecure", true, "Ignore server certificate if using https"),
+		ListeningAddress: flag.String("telemetry.address", ":9116", "Address on which to expose metrics."),
+		MetricsEndpoint:  flag.String("telemetry.endpoint", "/metrics", "Path under which to expose metrics."),
+		ClickhouseOnly:   flag.Bool("clickhouse_only", false, "Expose only Clickhouse metrics, not metrics from the exporter itself"),
+		Insecure:         flag.Bool("insecure", true, "Ignore server certificate if using https"),
+
 		ClickhouseScrapeURI: getEnv("CLICKHOUSE_URI", "http://127.0.0.1:8123"),
 		User:                getEnv("CLICKHOUSE_USER", "username"),
 		Password:            getEnv("CLICKHOUSE_PASSWORD", "password"),
+
+		QueryFiltersPath: getEnv("QUERY_FILTERS_PATH", "./conf/query-filters.yaml"),
 	}
 
 	// must be called after all flags are defined and before flags are accessed by the program
